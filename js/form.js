1f51f6ad2a8a6d00e64a0b1a94da2124ab8b64bc
nceyden <golikovdenis88@gmail.com>
2023-12-21 19:35:46 +0500

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -1,70 +1,100 @@
-
 import { isEscKey } from './utils.js';
+import { onFilterButtonChange, effectList, sliderWrapper } from './effects.js';
+
+const Zoom = {
+  MIN: 25,
+  MAX: 100,
+  STEP: 25,
+};
 
 const body = document.querySelector('body');
-const formUpload = document.querySelector('.img-upload__form');
-const fileUpload = document.querySelector('#upload-file');
-const uploadOverlay = document.querySelector('.img-upload__overlay');
-const closeButton = document.querySelector('#upload-cancel');
+const formUpload = body.querySelector('.img-upload__form');
+const overlay = body.querySelector('.img-upload__overlay');
+const fileUpload = body.querySelector('#upload-file');
+const formUploadClose = body.querySelector('#upload-cancel');
+const scaleButtonSmaller = body.querySelector('.scale__control--smaller');
+const scaleButtonBigger = body.querySelector('.scale__control--bigger');
+const scaleButtonValue = body.querySelector('.scale__control--value');
+const imagePreview = body.querySelector('.img-upload__preview img');
+const imagesEffectPreview = body.querySelectorAll('.effects__preview');
 
-// элементы для изменения изображений
-const effects = document.querySelectorAll('.effects__preview');
-const mainPicture = document.querySelector('.img-upload__preview img');
-
-// функция для закрытия формы
 const closeForm = () => {
-  uploadOverlay.classList.add('hidden');
+  overlay.classList.add('hidden');
   body.classList.remove('modal-open');
+  effectList.removeEventListener('change', onFilterButtonChange);
 
-  closeButton.removeEventListener('click', onCloseFormClick);
-  document.removeEventListener('keydown', onCloseFormEscDown);
+  imagePreview.style.transform = '';
+  imagePreview.className = 'img-upload__preview';
+  imagePreview.style.filter = '';
 
-  // сброс данных формы
   formUpload.reset();
 };
 
-// обработчик клика по кнопке закрытия формы
-function onCloseFormClick(evt) {
-  evt.preventDefault();
-  closeForm();
-}
-
-// обработчик нажатия клавиши Escape при открытой форме
-function onCloseFormEscDown(evt) {
+const onCloseFormEscKeyDown = (evt) => {
   if (isEscKey(evt) &&
-    !evt.target.classList.contains('text__hashtag') &&
-    !evt.target.classList.contains('text__description')) {
+      !evt.target.classList.contains('text__hashtags') &&
+      !evt.target.classList.contains('text__description')
+  ) {
     evt.preventDefault();
     closeForm();
-  }
-}
 
-// функция для изменения изображений при загрузке файла
+    document.removeEventListener('keydown', onCloseFormEscKeyDown);
+  }
+};
+
 const changeImages = () => {
   const file = fileUpload.files[0];
   const fileUrl = URL.createObjectURL(file);
 
-  // установка нового изображения для основного превью и эффектов
-  mainPicture.src = fileUrl;
+  imagePreview.src = fileUrl;
 
-  effects.forEach((effect) => {
-    effect.style.backgroundImage = `url('${fileUrl}')`;
-  });
+  for (const imageEffectPreview of imagesEffectPreview) {
+    imageEffectPreview.style.backgroundImage = `url(${fileUrl})`;
+  }
 };
 
-// обработчик изменения файла в поле загрузки
 const onFileUploadChange = () => {
-  // Отображение оверлея и блокировка прокрутки страницы
-  uploadOverlay.classList.remove('hidden');
+  overlay.classList.remove('hidden');
   body.classList.add('modal-open');
 
-  // изменение изображений
   changeImages();
 
-  // добавление обработчиков событий для закрытия формы
-  closeButton.addEventListener('click', onCloseFormClick);
-  document.addEventListener('keydown', onCloseFormEscDown);
+  document.addEventListener('keydown', onCloseFormEscKeyDown);
+  sliderWrapper.classList.add('hidden');
+  effectList.addEventListener('change', onFilterButtonChange);
 };
 
-// добавление обработчика события изменения файла
 fileUpload.addEventListener('change', onFileUploadChange);
+
+formUploadClose.addEventListener('click', () => {
+  closeForm();
+});
+
+const changeZoom = (coefficient) => {
+  const size = parseInt(scaleButtonValue.value, 10) + coefficient * Zoom.STEP;
+
+  scaleButtonValue.value = `${size}%`;
+  imagePreview.style.transform = `scale(${size / 100})`;
+};
+
+scaleButtonSmaller.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const coefficient = -1;
+
+  if (parseInt(scaleButtonValue.value, 10) > Zoom.MIN) {
+    changeZoom(coefficient);
+  }
+});
+
+scaleButtonBigger.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const coefficient = 1;
+
+  if (parseInt(scaleButtonValue.value, 10) < Zoom.MAX) {
+    changeZoom(coefficient);
+  }
+});
+
+export { imagePreview };
